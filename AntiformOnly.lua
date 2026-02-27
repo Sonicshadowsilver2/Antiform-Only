e9df8a525aafff583e3ebf8520359c820485e9bf
Sonicshadowsilver2 <danielctip2018@gmail.com>
2026-02-27 01:01:03 -0600

    Update

--- AntiformOnly.lua
+++ AntiformOnly.lua
@@ -66,17 +66,17 @@ function _OnFrame()
 		if ReadShort(Now+0x00) == 0x0102 and ReadByte(Now+0x08) == 0x34 then --New Game
 			WriteByte(Save+0x1CD9, 0x02) --Enable Square Button actions
 			WriteByte(Save+0x1CE5, 0x04) --Show Form Gauge
-			WriteInt(Save+0x32F4, 0x00000047) --Antiform Keyblade & Valor LV0
+			WriteShort(Save+0x32F6, 0x0000) --Valor LV0
 			WriteShort(Save+0x32FC, 0x0000) --Remove High Jump
 		end
 		if ReadByte(Now+0x00) == 0x02 then --Twilight Town
-			if ReadByte(Now+0x08) == 0x9D or ReadByte(Now+0x08) == 0x78 or ReadByte(Now+0x08) == 0x7D then
+			if ReadByte(Now+0x08) == 0x78 or ReadByte(Now+0x08) == 0x7D then
 				WriteShort(UCM+0x009C, 0x005A) --Antiform -> Roxas
 			else
 				WriteShort(UCM+0x009C, 0x0059) --Roxas -> Antiform
 			end
 			if ReadByte(Now+0x01) == 0x1C and ReadByte(Now+0x08) == 0x01 and ReadByte(Save+0x1CE5) == 0x05 then
-				WriteByte(Save+0x1CE5, 0x01)
+				WriteByte(Save+0x1CE5, 0x06)
 			end
 		end
 		if ReadByte(Now+0x00) == 0x0A then --Pride Lands
@@ -96,17 +96,8 @@ function _OnFrame()
 				end
 			end
 		end
-		if ReadByte(UCM+0x00) == 0x54 then
-			WriteShort(UCM+0x0000, 0x0059) --Sora (Normal) -> Antiform
-			WriteShort(UCM+0x00D0, 0x0059) --Roxas (Dual-Wielded) -> Antiform
-			WriteShort(UCM+0x0104, 0x0059) --Sora (KH1 Costume) -> Antiform
-			WriteShort(UCM+0x0444, 0x03EA) --Sora (Halloween Town) -> Antiform (Halloween Town)
-			WriteShort(UCM+0x0478, 0x095A) --Sora (Christmas Town) -> Antiform (Christmas Town)
-			WriteShort(UCM+0x0680, 0x0671) --Sora (Space Paranoids) -> Antiform (Space Paranoids)
-			WriteShort(UCM+0x06B4, 0x0672) --Sora (Timeless River) -> Antiform (Timeless River)
+		if ReadShort(Obj0+0x12698) == 0x4C41 then
 			WriteString(Obj0+0x12690,'P_EX100_HTLF\0\0\0\0\0') --Sora (On Carpet) Model -> Antiform Model
-			WriteString(Obj0+0x154D0,'F_TT010_SORA.mset\0') --Skateboard (Roxas) MSET -> Skateboard (Sora) MSET
-			WriteShort(Obj0+0x25B28,0x0000) --Remove Small Chests (Twilight Town)
 		end
 	end
 end
